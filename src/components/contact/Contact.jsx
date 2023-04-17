76141cc3e7480e525dfdbf89d024b19a8f82ac12
Victor Rivera <veektur@hotmail.com>
2023-04-18 01:44:07 +0200

    Change Contact and Experience class names

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './contact.css'
-import {MdOutlineEmail} from 'react-icons/md'
-import {RiMessengerLine} from 'react-icons/ri'
 import {FaWhatsapp} from 'react-icons/fa'
 import { useRef } from 'react';
 import emailjs from 'emailjs-com'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+
 
 const Contact = () => {
   const form = useRef();
@@ -18,23 +19,23 @@ const Contact = () => {
   };
   return (
     <section id='contact'>
-      <h5>Get In Touch</h5>
+      <h5>Get Connected</h5>
       <h2>Contact Me</h2>
       
-      <div className='container contact__container'>
-        <div className="contact_options">
-        <article className='contact__option'>
-            <MdOutlineEmail className='contact__option-icon'/>
+      <div className='container contact-wrapper'>
+        <div className="contact-method-wrapper">
+        <article className='contact-method'>
+            <MdOutlineEmail className='contact-method-icon'/>
             <h4>Email</h4>
             <h5>veektur@hotmail.com</h5>
             <a href="mailto:veektur@hotmail.com">Send a message</a>
-          </article><article className='contact__option'>
-            <RiMessengerLine className='contact__option-icon'/>
+          </article><article className='contact-method'>
+            <RiMessengerLine className='contact-method-icon'/>
             <h4>Messenger</h4>
             <h5>Victor Rivera</h5>
             <a href="https://m.me/Vrivera1993">Send a message</a>
-          </article><article className='contact__option'>
-            <FaWhatsapp className='contact__option-icon'/>
+          </article><article className='contact-method'>
+            <FaWhatsapp className='contact-method-icon'/>
             <h4>WhatsApp</h4>
             <h5>+49 15140016819</h5>
             <a href="https://api.whatsapp.com/send?phone=4915140016819">Send a message</a>
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -5,32 +5,32 @@ import {BsPatchCheckFill} from 'react-icons/bs'
 const Experience = () => {
   return (
     <section id='experience'>
-      <h5>What Skills I Have</h5>
+      <h5>My Skills</h5>
       <h2>My Experience</h2>
 
-      <div className='container experience__container'>
-        <div className="experience__frontend">
+      <div className='container exp-wrapper'>
+        <div>
           <h3>Frontend Development</h3>
-          <div className="experience__content">
-          <article className='experience__details'>
+          <div className="exp-content">
+          <article className='exp-description'>
             <BsPatchCheckFill className='experience-details-icon' />
             <div>
             <h4>HTML</h4>
             <small className='text-light'>Experienced</small>
             </div>
-          </article><article className='experience__details'>
+          </article><article className='exp-description'>
             <BsPatchCheckFill className='experience-details-icon'/>
             <div>
             <h4>CSS</h4>
             <small className='text-light'>Intermediate</small>
             </div>
-          </article><article className='experience__details'>
+          </article><article className='exp-description'>
             <BsPatchCheckFill className='experience-details-icon'/>
             <div>
             <h4>Javascript</h4>
             <small className='text-light'>Intermediate</small>
             </div>
-          </article><article className='experience__details'>
+          </article><article className='exp-description'>
             <BsPatchCheckFill className='experience-details-icon'/>
             <div>
             <h4>Bootstrap</h4>
@@ -41,19 +41,19 @@ const Experience = () => {
       </div>
       <div className='experience__backend'>
       <h3>Backend Development</h3>
-      <article className='experience__details'>
+      <article className='exp-description'>
         <BsPatchCheckFill className='experience-details-icon'/>
         <div>
         <h4>Node JS</h4>
         <small className='text-light'>Experienced</small>
         </div>
-        </article><article className='experience__details'>
+        </article><article className='exp-description'>
           <BsPatchCheckFill className='experience-details-icon'/>
           <div>
           <h4>MongoDB</h4>
           <small className='text-light'>Intermediate</small>
           </div>
-        </article><article className='experience__details'>
+        </article><article className='exp-description'>
           <BsPatchCheckFill className='experience-details-icon'/>
           <div>
           <h4>MySQL</h4>
